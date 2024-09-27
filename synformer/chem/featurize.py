3c19adc34993e4db8d970c3a94223824ee4a36f9
wenhao-gao <gaowh19@gmail.com>
2024-09-27 18:44:32 -0400

    upload

--- synformer/chem/featurize.py
+++ synformer/chem/featurize.py
@@ -25,12 +25,162 @@ def bond_features_simple(bond: Chem.rdchem.Bond | None) -> int:
     return 5
 
 
-_smiles_vocab_path = Path(__file__).parent / "smiles_vocab.txt"
-with open(_smiles_vocab_path) as f:
-    _smiles_vocab = f.read().splitlines()
-    _smiles_token_to_id = {token: i for i, token in enumerate(_smiles_vocab, start=1)}
-    _smiles_token_max = max(_smiles_token_to_id.values())
-    _smiles_token_pattern = re.compile("(" + "|".join(map(re.escape, sorted(_smiles_vocab, reverse=True))) + ")")
+_smiles_vocab = """H
+He
+Li
+Be
+B
+C
+N
+O
+F
+Ne
+Na
+Mg
+Al
+Si
+P
+S
+Cl
+Ar
+K
+Ca
+Sc
+Ti
+V
+Cr
+Mn
+Fe
+Co
+Ni
+Cu
+Zn
+Ga
+Ge
+As
+Se
+Br
+Kr
+Rb
+Sr
+Y
+Zr
+Nb
+Mo
+Tc
+Ru
+Rh
+Pd
+Ag
+Cd
+In
+Sn
+Sb
+Te
+I
+Xe
+Cs
+Ba
+La
+Ce
+Pr
+Nd
+Pm
+Sm
+Eu
+Gd
+Tb
+Dy
+Ho
+Er
+Tm
+Yb
+Lu
+Hf
+Ta
+W
+Re
+Os
+Ir
+Pt
+Au
+Hg
+Tl
+Pb
+Bi
+Po
+At
+Rn
+Fr
+Ra
+Ac
+Th
+Pa
+U
+Np
+Pu
+Am
+Cm
+Bk
+Cf
+Es
+Fm
+Md
+No
+Lr
+Rf
+Db
+Sg
+Bh
+Hs
+Mt
+Ds
+Rg
+Cn
+Nh
+Fl
+Mc
+Lv
+Ts
+Og
+b
+c
+n
+o
+s
+p
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+[
+]
+(
+)
+.
+=
+#
+-
++
+\\
+/
+:
+~
+@
+?
+>
+*
+$
+%""".splitlines()
+_smiles_token_to_id = {token: i for i, token in enumerate(_smiles_vocab, start=1)}
+_smiles_token_max = max(_smiles_token_to_id.values())
+_smiles_token_pattern = re.compile("(" + "|".join(map(re.escape, sorted(_smiles_vocab, reverse=True))) + ")")
 
 
 def tokenize_smiles(s_in: str):
